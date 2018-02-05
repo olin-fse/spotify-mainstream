0562e0a4f361611c32a71d3cd129441be6cbe658
kghite <kghite@gmail.com>
2018-02-05 14:43:57 -0500

    ADD: Working mysql connection

---
+++ StructureDocumentation.md
@@ -0,0 +1,16 @@
+# Structure and Components
+
+## Database
+
+**Overview:**
+Using mySQL hosted on Amazon RDS.  [Setup Tutorial](https://aws.amazon.com/getting-started/tutorials/create-mysql-db/)
+
+**Database Information:** 
+
+* spotify-mainstream-main - Relational database server name
+* spotify_mainstream_dev - Development database instance name
+* Port 3306
+* Tables:
+	* users
+
+**Structure:**
--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// TODO --> put database connection here
+// Connect to database and log tables to confirm
+var DB_CONFIG = require('./config/database_config.secret.json');
+connection = mysql.createConnection({
+	host: DB_CONFIG.host,
+	port: DB_CONFIG.port,
+	user: DB_CONFIG.login,
+	password: DB_CONFIG.pass,
+	database: DB_CONFIG.database
+});
 
+connection.connect(function(err) { 
+	if (err) { 
+		console.error('Error connecting to mysql: ' + err.stack);
+		return;
+	}
+	console.log('Connected to database as ' + connection.threadID);
+});
+
+var sql_command = "show tables";
+connection.query(sql_command, function (err, res) {
+	if (err) {
+		console.error('Error getting database tables: ' + err.stack);
+	}
+	console.log("Found database tables: " + JSON.stringify(res));
+});
 
 // TODO --> put the routes into the ./routes folder/files
 // TODO --> also, write tests for all of these!
